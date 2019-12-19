108c4a8d7b410e565f1acd7131bbda1c9010b389
kleyson castro <kleysoncastro99@gmail.com>
2019-12-19 13:01:35 -0300

    chore: page select hour and provider

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Bachground from '~/components/Background';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function SelectProvider() {
-  return <Bachground />;
+import { Container, ProviderList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, SetProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProvider() {
+      const response = await api.get('/provider');
+
+      SetProviders(response.data);
+    }
+
+    loadProvider();
+  }, []);
+  return (
+    <Bachground>
+      <Container>
+        <ProviderList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider onPress={() => navigation.navigate('SelectDateTime')}>
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Bachground>
+  );
 }
 SelectProvider.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o prestador',
